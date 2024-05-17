5814199f5eac28ecfc7e970c5054f6e201620008
Jose-17-11 <ariashdez@outlook.com>
2024-05-17 16:06:09 -0600

    add portada

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eledoce.github.io/',
-  base: '/pacientraker/',
+  // site: 'https://eledoce.github.io/',
+  // base: '/pacientraker/',
   integrations: [tailwind()]
 });
